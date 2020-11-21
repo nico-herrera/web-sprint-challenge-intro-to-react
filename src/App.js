e30261dc333c1b91ab86c173ac16927d0ae24bc3
Nico Herrera <nherrera0831@gmail,com>
2020-11-20 20:42:54 -0800

    finished putting data on screen

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Character from "./components/Character";
+import axios from "axios";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const [characterList, setCharacterList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const characterList = await axios(
+          "https://rickandmortyapi.com/api/character/"
+        );
+        console.log(characterList);
+        setCharacterList(characterList.data);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>No info for today!</div>;
+  }
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character characterData={characterList} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,65 @@
 // Write your Character component here
+import React from "react";
+
+import styled from "styled-components";
+
+export default function Character(props) {
+  const { characterData } = props;
+
+  console.log(characterData);
+
+  const StyledContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: space-evenly;
+    align-items: center;
+  `;
+
+  const StyledCharacterBox = styled.div`
+    height: 150px;
+    width: 200px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border: 2px solid black;
+  `;
+
+  const StyledName = styled.h3`
+    font-size: 1.1rem;
+    text-align: center;
+    color: white;
+    background-color: black;
+  `;
+
+  const StyledStatus = styled.p`
+    font-size: 0.8rem;
+    text-align: center;
+    color: white;
+    background-color: black;
+  `;
+
+  const StyledSpecies = styled.p`
+    font-size: 1rem;
+    text-align: center;
+    color: white;
+    background-color: black;
+  `;
+  //   console.log(characterData.data.info.results);
+
+  return (
+    <StyledContainer>
+      {characterData.results.map((character, index) => {
+        return (
+          <StyledCharacterBox>
+            <StyledName>{character.name}</StyledName>
+            <StyledStatus> {character.status}</StyledStatus>
+            <StyledSpecies> {character.species}</StyledSpecies>
+          </StyledCharacterBox>
+        );
+      })}
+    </StyledContainer>
+  );
+}
